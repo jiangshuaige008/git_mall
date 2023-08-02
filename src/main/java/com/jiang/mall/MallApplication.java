536ebc859fa327e08d098c16bf05fbe9a33b4e88
jiang <1151547031@qq.com>
2023-08-02 18:45:36 +0800

    hot fix commit1

--- src/main/java/com/jiang/mall/MallApplication.java
+++ src/main/java/com/jiang/mall/MallApplication.java
@@ -11,6 +11,7 @@ public class MallApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(MallApplication.class, args);
+        System.out.println("hell.git4");
         Object jiang = run.getBean("jiang");
 
     }
